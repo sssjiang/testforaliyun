9d9fb98a6f2bb7d7642eed38bbf1a01d19551255
sssjiang <33194239+sssjiang@users.noreply.github.com>
2023-10-03 13:46:13 +0800

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -2,7 +2,13 @@ if [ ! -d ~/pythonenv ]; then
     mkdir -p ~/pythonenv
 
 fi
+echo "Current directory: $(pwd)"
 cd ~/pythonenv
 python3 -m venv env
 source env/bin/activate
 pip3 install -r requirements.txt
+if [ -n "$VIRTUAL_ENV" ]; then
+    echo "Python virtual environment is activated."
+else
+    echo "Python virtual environment is not activated."
+fi
