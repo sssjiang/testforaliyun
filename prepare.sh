9d6cfe7af432bbee9eadd4d52deb3d3aa1c457fa
sssjiang <33194239+sssjiang@users.noreply.github.com>
2023-10-03 13:35:25 +0800

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,8 @@
 if [ ! -d ~/pythonenv ]; then
     mkdir -p ~/pythonenv
-    cd ~/pythonenv
-    python3 -m venv env
-    source env/bin/activate
-    pip3 install -r requirements.txt
+
 fi
+cd ~/pythonenv
+python3 -m venv env
+source env/bin/activate
+pip3 install -r requirements.txt
